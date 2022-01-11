e407c63a9f3e79f548bc74b7317de55bbad85756
MarceloQuiroga <j_quiroga@fpzornotza.com>
2022-01-11 12:53:43 +0100

    update

---
+++ model/connect_data_local.php
@@ -0,0 +1,11 @@
+<?php
+
+
+class connect_data
+{
+    public $host='localhost';
+    public $userbbdd='root';
+    public $passbbdd='';
+    public $ddbbname='reto_covid';
+}
+
---
+++ model/connect_data_serv.php
@@ -0,0 +1,11 @@
+<?php
+
+
+class connect_data
+{
+    public $host='localhost';
+    public $userbbdd='hiruzerbi_root';
+    public $passbbdd='password';
+    public $ddbbname='reto_covid';
+}
+
