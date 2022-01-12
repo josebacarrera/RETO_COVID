59c5ac2324627b3c9b98e1e41a5bc28f97406f1f
LoreaH9 <l_hernandez@fpzornotza.com>
2022-01-12 10:36:08 +0100

    todo bien

--- model/datosPacienteModel.php
+++ model/datosPacienteModel.php
@@ -7,29 +7,33 @@ if ($_SERVER['SERVER_NAME']== "hiru.zerbitzaria.net") {
     include_once ("connect_data_LOCAL.php");
 }
 
-include_once("citaClass.php");
+include_once("datosPacienteClass.php");
 
 class datosPacienteModel extends datosPacienteClass{
 
-    private $link;
-
-    //enlace con la base de datos
-
-    public function OpenConnect(){
-        $kondat=new connect data();
-        try{
-            $this->link=new mysqli($kondat->host,$kondat->userbbdd,$kondat->passbbdd,$kondat->ddbbname);
-            // creamos el objeto link en la clase mysqli con la informacion de conexion
-        }
-        catch(Exception $e){
-            echo $e->getMessage();
-        }
-        $this->link->set_charset("utf8");
-        // forzamos el uso de utf8 para el intercambio de datos entre la aplicacion y la base de datos
-
+    private $links;
+  
+public function OpenConnect()
+{
+    $konDat=new connect_data();
+    try
+    {
+        $this->link=new mysqli($konDat->host,$konDat->userbbdd,$konDat->passbbdd,$konDat->ddbbname);
     }
-    public function CloseConnect() {
-        mysqli_close ($this->link);
+    catch(Exception $e)
+    {
+        echo $e->getMessage();
     }
+    $this->link->set_charset("utf8"); // honek behartu egiten du aplikazio eta
+    //                  //databasearen artean UTF -8 erabiltzera datuak trukatzeko
+}
+
+public function CloseConnect()
+{
+    mysqli_close ($this->link);
+}
+
+
+
 }
 ?>
