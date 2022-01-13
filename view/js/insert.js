b0d203d6033493df4c06cb652daf2c93c332f036
Lorea <hernandez_lorea@eitb.eus>
2022-01-13 15:47:10 +0100

    navbar en todas las paginas

--- view/js/insert.js
+++ view/js/insert.js
@@ -1,5 +1,11 @@
 $(document ).ready(load)
+var reto_covid = angular.module('reto_covid', []);
+reto_covid.controller('navbarController', ($scope) => {
+    $scope.prueba=function name(params) {
+        console.log($scope + "holaa")
+    } 
 
+});
 function load(){
     $("#navbar").load("view/shared/navbar.html");
     $("#footer").load("view/shared/footer.html");
