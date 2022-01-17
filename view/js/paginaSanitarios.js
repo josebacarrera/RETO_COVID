8142c7bb4b11159c6cb613a3bd79e870539b7ab5
Lorea <hernandez_lorea@eitb.eus>
2022-01-17 16:51:29 +0100

    paginaSalud

--- view/js/paginaSanitarios.js
+++ view/js/paginaSanitarios.js
@@ -1,7 +1,23 @@
+$(document).ready(init);
+
+function init() {/* 
+    //Comprueba que se ha iniciado sesión
+    await getSession().then(async function(session){
+
+   });*/
+    loadContent();
+}
+
+function loadContent() {
+    $("#datosPersonales").click(function () {
+        $(".infoOpcion").css('display', 'none')
+        $("#infoDatosPersonales").css('display', 'block')
+    });
+}
 var paginaSanitarios = angular.module('paginaSanitarios', []);
 
 
 paginaSanitarios.controller('datosPersonales', function ($scope) {
-    $scope.nombre="prueba";
+    $scope.nombre = "prueba";
     $('#nombreTrabajador').val($scope.nombre);
 });
