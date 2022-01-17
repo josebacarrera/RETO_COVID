9c916d4040f4f2c64a8be7afaefc60dd88c37b43
ekaitz1337 <ekaitz.alvarez34@gmail.com>
2022-01-17 14:45:54 +0100

    12

--- view/js/paginaUser.js
+++ view/js/paginaUser.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(){
-    elementoAgenda=document.getElementById("#mostrarAgenda");
+    elementoAgenda=document.getElementById("mostrarAgenda");
     elementoAgenda.addEventListener("click", showAgenda);
-    elementoInforme=document.getElementById("#mostrarInforme");
+    elementoInforme=document.getElementById("mostrarInforme");
     elementoInforme.addEventListener("click", showInforme);
-    elementoDatos=document.getElementById("#mostrarDatos");
+    elementoDatos=document.getElementById("mostrarDatos");
     elementoDatos.addEventListener("click", showDatos);
+    elementoDatos=document.getElementById("mostrarCovid");
+    elementoDatos.addEventListener("click", showCovid);
 });
 
 function showAgenda(){
@@ -18,13 +20,13 @@ function showInforme(){
     document.getElementById("mostrarInforme").style.display = '';
     document.getElementById("mostrarDatos").style.display = 'none';
     document.getElementById("mostrarCovid").style.display = 'none';
+    console.log("hawaii");
 }
 function showDatos(){
     document.getElementById("mostrarAgenda").style.display = 'none';
     document.getElementById("mostrarInforme").style.display = 'none';
     document.getElementById("mostrarDatos").style.display = '';
     document.getElementById("mostrarCovid").style.display = 'none';
-    console.log(hawaii);
 }
 function showCovid(){
     document.getElementById("mostrarAgenda").style.display = 'none';
