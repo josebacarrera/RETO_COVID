d9bcfef1bdbf55538b1edceef29c1a471e6fde73
MarceloQuiroga <J_quiroga@fpzornotza.com>
2022-01-18 15:24:02 +0100

    update

--- model/datosPacienteModel.php
+++ model/datosPacienteModel.php
@@ -1,11 +1,7 @@
 <?php
 
-
-if ($_SERVER['SERVER_NAME']== "hiru.zerbitzaria.net") {
-    include_once ("connect_data_SERV.php");
-} else {
-    include_once ("connect_data_LOCAL.php");
-}
+if ($_SERVER['SERVER_NAME']== "hiru.zerbitzaria.net") {include_once ("connect_data_SERV.php");} 
+else {include_once ("connect_data_LOCAL.php");}
 
 include_once("datosPacienteClass.php");
 include_once("citaModel.php");
@@ -19,8 +15,8 @@ class datosPacienteModel extends datosPacienteClass{
     private $objCita;
     private $objVacunacion;
     private $objVacuna;
+    private $objCentro;
 
-    //enlace con la base de datos
     public function OpenConnect(){
         $kondat=new connect_data();
         try{
@@ -39,51 +35,66 @@ class datosPacienteModel extends datosPacienteClass{
         mysqli_close ($this->link);
     }
 
+    // FUNCIONES MOD //
+
     public function loginTIS() {
 
         $this->OpenConnect();  
         
-        // $sql = "SELECT *
-        //         FROM datos_paciente d
-        //         INNER JOIN localidad l ON l.cod = d.cod_localidad
-        //         INNER JOIN centro c ON c.cod_localidad = l.cod
-        //         INNER JOIN cita ci ON ci.tis_paciente = d.tis
-        //         INNER JOIN registro_vacunacion r ON r.tis = d.tis
-        //         INNER JOIN vacuna v ON v.cod = r.cod_vacuna
-        //         WHERE d.tis ='" . $this->getTis() . "'  AND d.fecha_nacimiento = '" . $this->getFecha_nacimiento() . "';"; 
-        
-                
-        $sql = "SELECT *
-                FROM datos_paciente d
-                INNER JOIN localidad l ON l.cod = d.cod_localidad
-                INNER JOIN centro c ON c.cod_localidad = l.cod
-                INNER JOIN cita ci ON ci.tis_paciente = d.tis
-                INNER JOIN registro_vacunacion r ON r.tis = d.tis
-                INNER JOIN vacuna v ON v.cod = r.cod_vacuna
-                WHERE d.tis ='4990916'  AND d.fecha_nacimiento = '2018-03-17';";
+        // $sql = "CALL spLoginTIS('" . $this->getTis() . "','" . $this->getFecha_nacimiento() . "')";            
+        $sql = "CALL spLoginTIS('4990916','2018-03-17')";
 
         $result = $this->link->query($sql);
         
         if ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
             
-            $this->setNombre($row['nombre']);
-            $this->setApellido($row['apellido']);
-            $this->setEmail($row['email']);
-            $this->setFoto_perfil($row['foto_perfil']);
-            $this->setDireccion($row['direccion']);
-            $this->setCod_localidad($row['cod_localidad']);
+            $this->setNombre($row['nombre_p']);
+            $this->setApellido($row['apellido_p']);
+            $this->setEmail($row['email_p']);
+            $this->setFoto_perfil($row['foto_perfil_p']);
+            $this->setDireccion($row['direccion_p']);
+            $this->setCod_localidad($row['cod_localidad_p']);
 
             $this->objLocalidad = new localidadModel();
-            $this->objLocalidad->setCod($row['cod_localidad']);
-            $this->objLocalidad->setNombre($row['localidad.nombre']);
+            $this->objLocalidad->setCod($row['cod_localidad_l']);
+            $this->objLocalidad->setNombre($row['nombre_l']);
             $this->objLocalidad = $this->objLocalidad->ObjVars();
 
+            $this->objCentro = new centroModel();
+            $this->objCentro->setCod($row['cod_centro_ce']);
+            $this->objCentro->setCod_localidad($row['cod_localidad_ce']);
+            $this->objCentro->setNombre($row['nombre_ce']);
+            $this->objCentro->setTelefono($row['telefono_ce']);
+            $this->objCentro->setEmail($row['email_ce']);
+            $this->objCentro->setHorario_temprano($row['horario_temprano_ce']);
+            $this->objCentro->setHorario_tarde($row['horario_tarde_ce']);
+            $this->objCentro->setHorario_noche($row['horario_noche_ce']);
+            $this->objCentro->setDias($row['dias_ce']);
+            $this->objCentro = $this->objCentro->ObjVars();
+
             $this->objCita = new citaModel();
-            $this->objCita->setTis_paciente('4990916');
-            $this->objCita = $this->objCita->getCitaByTis();
+            $this->objCita->setCod($row['cod_cita_ci']);
+            $this->objCita->setTis_paciente($row['tis_paciente_ci']);
+            $this->objCita->setCod_sanitario($row['cod_sanitario_ci']);
+            $this->objCita->setFecha($row['fecha_ci']);
+            $this->objCita->setHora($row['hora_ci']);
+            $this->objCita->setCod_centro($row['cod_centro_ci']);
+            $this->objCita = $this->objCita->ObjVars();
 
             $this->objVacunacion = new registroVacunacionModel();
-            $this->objVacunacion->setTis('4990916');
+            $this->objVacunacion->setCod($row['cod_registro_rg']);
+            $this->objVacunacion->setTis($row['tis_registro_rg']);
+            $this->objVacunacion->setCod_vacuna($row['cod_vacuna_rg']);
+            $this->objVacunacion->setDosis($row['dosis_rg']);
+            $this->objVacunacion->setFecha_ultima_vacuna($row['fecha_ultima_vacuna_rg']);
+            $this->objVacunacion = $this->objVacunacion->ObjVars();
+
+            $this->objVacuna = new vacunaModel();
+            $this->objVacuna->setCod($row['cod_vacuna_v']);
+            $this->objVacuna->setNombre($row['nombre_v']);
+            $this->objVacuna->setMax($row['max_v']);
+            $this->objVacuna->setIntervalo($row['intervalo_v']);
+            $this->objVacuna = $this->objVacuna->ObjVars();
 
             return true;
 
--- model/localidadModel.php
+++ model/localidadModel.php
@@ -1,9 +1,8 @@
 <?php
-if ($_SERVER['SERVER_NAME']== "hiru.zerbitzaria.net") {
-    include_once ("connect_data_SERV.php");
-} else {
-    include_once ("connect_data_LOCAL.php");
-}
+
+if ($_SERVER['SERVER_NAME']== "hiru.zerbitzaria.net") {include_once ("connect_data_SERV.php");} 
+else {include_once ("connect_data_LOCAL.php");}
+
 include_once("localidadClass.php");
 
 class localidadModel extends localidadClass{
