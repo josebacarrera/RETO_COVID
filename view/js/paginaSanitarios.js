764424350e6586b031c2e7be563ddf4ddc749193
LoreaH9 <l_hernandez@fpzornotza.com>
2022-01-18 10:21:17 +0100

    si

--- view/js/paginaSanitarios.js
+++ view/js/paginaSanitarios.js
@@ -10,8 +10,19 @@ function init() {/*
 
 function loadContent() {
     $("#datosPersonales").click(function () {
-        $(".infoOpcion").css('display', 'none')
-        $("#infoDatosPersonales").css('display', 'block')
+        $(".infoOpcion").css('display', 'none');
+        $("#infoDatosPersonales").css('display', 'block');
+
+    });
+    $("#agendaSanitaria").click(function () {
+        $(".infoOpcion").css('display', 'none');
+        $("#infoAgenda").css('display', 'block');
+
+    });
+    $("#informes").click(function () {
+        $(".infoOpcion").css('display', 'none');
+        $("#infoInformes").css('display', 'block');
+
     });
 }
 var paginaSanitarios = angular.module('paginaSanitarios', []);
@@ -20,4 +31,26 @@ var paginaSanitarios = angular.module('paginaSanitarios', []);
 paginaSanitarios.controller('datosPersonales', function ($scope) {
     $scope.nombre = "prueba";
     $('#nombreTrabajador').val($scope.nombre);
+    $scope.editableInput = false;
+
+    $scope.btnGuardar = function () {
+
+    };
+
+});
+
+//Activar los campos para editarlos
+paginaSanitarios.directive("inputDisabled", function () {
+    return function (scope, element, attrs) {
+        scope.$watch(attrs.inputDisabled, function (val) {
+            if (val) {
+                element.removeAttr("disabled");
+                $("#btnGuardar").css('display','block')
+            }
+            else {
+                element.attr("disabled", "disabled");
+                $("#btnGuardar").css('display','none')
+            }
+        });
+    }
 });
--- view/js/script.js
+++ view/js/script.js
@@ -2,13 +2,22 @@ $(document).ready(init);
 var reto_covid = angular.module('reto_covid', []);
 
 async function init() {
-    await getSession().then(async function(session){
-         loadUser(session);
+    await getSession().then(async function (session) {
+        loadUser(session);
     });
-    
+
 }
 function loadUser(session) {
     console.log(session);
+    console.log(session.nombre_sanitario)
+    $('#formLogin').css('text-align', 'center')
+    $('#formLogin').css('padding', '30px')
+    if (session.nombre_sanitario) {
+        $('#formLogin').html('<p> <u> Nombre y apellido:</u> ' + session.nombre_sanitario + ' ' + session.nombre_sanitario + '</p>' +
+            '<p> <u> DNI:</u> ' + session.dni_sanitario + '</p>' +
+            '<p><u>Cargo: </u>' + session.cargo_sanitario + '</p>')
+        $('#formLogin').append('<button class="btn btn-primary" type="button">Log Out</button>')
+    }
 }
 reto_covid.controller('loginController', function ($scope) {
 
@@ -19,22 +28,22 @@ reto_covid.controller('loginController', function ($scope) {
 
         var url = "controller/cLogin.php";
         var data = {
-                        'solicitud':'loginDni',
-                        'usuario':$scope.usuario,
-                        'password':$scope.password
-                    };
+            'solicitud': 'loginDni',
+            'usuario': $scope.usuario,
+            'password': $scope.password
+        };
 
         fetch(url, {
-              method: 'POST',
-              body: JSON.stringify(data),
-              headers:{'Content-Type': 'application/json'}  
-              
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: { 'Content-Type': 'application/json' }
+
         }).then(res => res.json()).then(result => {
-    
+
             console.log(result);
-    
+
         }).catch(error => console.error('Error status:', error));
-        
-        return false;    
+
+        return false;
     };
 });
