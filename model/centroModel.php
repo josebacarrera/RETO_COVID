2087d7f252c8c5d53e9a4830358491221773e8db
ekaitz1337 <ekaitz.alvarez34@gmail.com>
2022-01-19 13:24:11 +0100

    recemos

--- model/centroModel.php
+++ model/centroModel.php
@@ -1,10 +1,7 @@
 <?php
 
-if ($_SERVER['SERVER_NAME']== "hiru.zerbitzaria.net") {
-    include_once ("connect_data_SERV.php");
-} else {
-    include_once ("connect_data_LOCAL.php");
-}
+if ($_SERVER['SERVER_NAME']== "hiru.zerbitzaria.net") {include_once ("connect_data_SERV.php");} 
+else {include_once ("connect_data_LOCAL.php");}
 
 include_once 'centroClass.php';
 
