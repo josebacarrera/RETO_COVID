6662176fb4291620c4851be0830ac85fe0f53ed8
LoreaH9 <l_hernandez@fpzornotza.com>
2022-02-01 10:43:42 +0100

    aaa

--- model/connect_data_serv.php
+++ model/connect_data_serv.php
@@ -6,6 +6,6 @@ class connect_data
     public $host='localhost';
     public $userbbdd='hiruzerb_root';
     public $passbbdd='ikaslea_123';
-    public $ddbbname='reto_covid';
+    public $ddbbname='hiruzerb_reto_covid';
 }
 
